5bee42f6a1b9a18f03706a80aa0a0918aa97f95a
John D. Riepe <JohnD.Riepe@Danielles-MacBook-Pro.local>
2016-07-07 12:37:10 -0400

    unable to figure out the combo of handlebars +  orm etc

--- config/orm.js
+++ config/orm.js
@@ -6,20 +6,20 @@ var orm = {
     selectAll: function(tableSelected) {
         var queryString = 'SELECT * FROM ' + tableSelected;
         connection.query(queryString, function(err, result) {
-            console.log(result);
+            console.log(result);       
         });
     },
     insertOne: function(tableToInsert, burgName) {
         var queryString = 'INSERT INTO ' + tableToInsert + ' (burger_name, devoured, date) VALUES (' + burgName + ', false, now())';
-        console.log(queryString)
+        //console.log(queryString)
         connection.query(queryString, function(err, result) {
-            console.log(result);
+            //console.log(result);
         });
     },
     updateOne: function(tableToUpdate, valKey) {
-        var queryString = 'UPDATE ' + tableToUpdate + ' SET devoured = false WHERE id = ' + valKey;
-        connection.query(queryString, function(err, result) {
-            console.log(result);
+        var queryString = 'UPDATE ' + tableToUpdate + ' SET devoured = false WHERE id = ?';
+        connection.query(queryString, [valKey], function(err, result) {
+            //console.log(result);
         });
     }
 };
