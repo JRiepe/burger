431af3475b5dbdb503182f16a230efc4c196e120
John D. Riepe <JohnD.Riepe@Danielles-MacBook-Pro.local>
2016-07-07 12:43:51 -0400

    changed orm.js

--- config/orm.js
+++ config/orm.js
@@ -6,20 +6,20 @@ var orm = {
     selectAll: function(tableSelected) {
         var queryString = 'SELECT * FROM ' + tableSelected;
         connection.query(queryString, function(err, result) {
-            console.log(result);
+            return result;
         });
     },
     insertOne: function(tableToInsert, burgName) {
         var queryString = 'INSERT INTO ' + tableToInsert + ' (burger_name, devoured, date) VALUES (' + burgName + ', false, now())';
         console.log(queryString)
         connection.query(queryString, function(err, result) {
-            console.log(result);
+            return result;
         });
     },
     updateOne: function(tableToUpdate, valKey) {
         var queryString = 'UPDATE ' + tableToUpdate + ' SET devoured = false WHERE id = ' + valKey;
         connection.query(queryString, function(err, result) {
-            console.log(result);
+            return result;
         });
     }
 };
